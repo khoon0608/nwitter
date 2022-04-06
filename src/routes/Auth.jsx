08c1af6efaab3e1909ae3302da31a17e271d5399
khoon0608 <khoon0608@gmail.com>
2022-04-06 13:57:56 +0900

    2.3 계정생성

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,11 +1,73 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { authService } from "fBase";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+
+    if (name === "email") setEmail(value);
+    else if (name === "password") setPassword(value);
+  };
+  const onSubmit = async(event) => {
+    event.preventDefault();
+    try{
+      let data = null;
+      if (newAccount) {
+         data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        )
+        // create account
+      } else {
+         data = await signInWithEmailAndPassword(
+          authService,
+          email,
+          password
+        )
+        // log in
+      }
+      console.log(data);
+    } catch(error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
-      <span>Auth</span>
+      <form onSubmit={onSubmit}>
+        <input
+          name='email'
+          type='email'
+          placeholder='Email'
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name='password'
+          type='text'
+          placeholder='Password'
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <button> {newAccount ? "Create Account" : "Log In"} </button>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
     </div>
   );
 };

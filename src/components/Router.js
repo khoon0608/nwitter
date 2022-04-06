827b0573c1ff2cd4bb5fdeac0393cd60882f7a47
khoon0608 <khoon0608@gmail.com>
2022-04-06 17:20:38 +0900

    로그아웃 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,21 +2,28 @@
 
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path='/'>
               <Home />
             </Route>
+            <Route exact path='/profile'>
+              <Profile />
+            </Route>
           </>
         ) : (
           // Fragment: 부모요소는 없지만 많은 요소들을 render하고 싶을 때 사용하는 빈 엘리먼트
+
           <Route exact path='/'>
             <Auth />
           </Route>
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,12 +1,19 @@
 /** @format */
 
 import React from "react";
+import { authService } from "fBase";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Profile = () => {
+  const history = useHistory();
+  const logOutFunc = () => {
+    authService.signOut();
+    history.push("/");
+  }
   return (
-    <div>
-      <span>Profile</span>
-    </div>
+    <>
+      <button onClick={logOutFunc}>Log Out</button>
+    </>
   );
 };
 
